f3baaa64cb475ec1506f41a52f1f4311130964c6
Glitch (corgi-button) <none>
2018-06-27 12:05:57 +0000

    🔮🍑 Updated with Glitch

--- src/index.js
+++ src/index.js
@@ -11,9 +11,22 @@ const actions = {
 }
 
 const view = state => 
-  <div>
-    <button onclick={action("fetchCorgi")}>New Corgi!</button>
-    <img src={state.corgi} alt=""/>
+  <div className="page">
+    <div className="header">
+      <h2>The Corgi Generator</h2>
+    </div>
+    <div className="image">
+      <img src={state.corgi} alt=""/>
+    </div>
+    <div className="button">
+      <button className="button__corgi" onclick={action("fetchCorgi")}>New Corgi!</button>
+    </div>
+    <div className="footer">
+      <p>
+       Corgis provided by <a href="https://dog.ceo/dog-api/">dog.ceo API</a>. 
+       Code @<a href="https://glitch.com/edit/#!/corgi-button">Glitch</a>.
+      </p>
+    </div>
   </div>
 
 withFx(app)(state, actions, view, document.body).fetchCorgi()
